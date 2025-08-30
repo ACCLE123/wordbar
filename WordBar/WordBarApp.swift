c23250432a3649f7f809a58dbba11ed6085d1f4e
accle <3196385390@qq.com>
2025-08-30 16:34:31 +0800

    Initial Commit

---
+++ WordBar/WordBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordBarApp.swift
+//  WordBar
+//
+//  Created by yangqi on 2025/8/30.
+//
+
+import SwiftUI
+
+@main
+struct WordBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordBarTests/WordBarTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordBarTests.swift
+//  WordBarTests
+//
+//  Created by yangqi on 2025/8/30.
+//
+
+import Testing
+@testable import WordBar
+
+struct WordBarTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
